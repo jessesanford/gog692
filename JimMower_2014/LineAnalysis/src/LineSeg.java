55fbe7613c3d123a99f1e29db7d3b67a2da40e88
joshsanford <joshsanford@gmail.com>
2014-02-22 11:38:23 -0500

    updated to latest work from sat

--- JimMower_2014/LineAnalysis/src/LineSeg.java
+++ JimMower_2014/LineAnalysis/src/LineSeg.java
@@ -1,30 +1,60 @@
-
+import java.awt.*;
 public class LineSeg {
 	private Point2f P1;
 	private Point2f P2;
 	private float a;
 	private float b;
 	private float c;
-	private float length;
+	private double length;
 	
-	
-	public LineSeg(Point2f myP1, Point2f myP2) {
-		P1 = myP1; 
-		P2 = myP2;
-		calculateCoefficients(myP1, myP2);
+	public LinSeg() {
+		P1 = new Point2f();
+		P2 = new Point2f();
+		length = 0;
 	}
 	
-	private void calculateCoefficients(Point2f myPoint1, Point2f myPoint2) {
-		float x1 = myPoint1.getX();
-		float x2 = myPoint2.getX();
-		float y1 = myPoint1.getY();
-		float y2 = myPoint2.getY();
-		
-		this.a = y1 - y2;
-		this.b = x2 - x1;
-		this.c = (x1 * y2) - (x2 * y1);
-		this.length = (float) Math.sqrt((a * a) + (b * b)); 	
-		
+	public LineSeg(int startx, int starty, int endx, int endy) {
+		P1 = new Point2f(startx, starty);
+		P2 = new Point2f(endx, endy);
+		calculateCoefficients();
+	}
+	public LineSeg(Point2f s, Point2f e) {
+		P1 = s;
+		P2 = e;
+		calculateCoefficients();
+	}
+	private void calculateCoefficients() {
+		a = P1.getY() - P2.getY();
+		b = P2.getX() - P1.getX();
+		c = P1.getX() * P2.getY() - P2.getX() * P1.getY();
+		length = (float)Math.sqrt(a*a + b*b);	
+	}
+	
+	public Point2f getP1() {
+		return P1;
+	}
+	public Point2f getP2() {
+		return P2;
+	}
+	
+	public void setP1(int x, int y) {
+		P1 = new Point2f(x,y);
+		calculateCoefficients();
+	}
+	
+	public void setP1(Point2f p) {
+		P1 = p;
+		calculateCoefficients();
+	}
+	
+	public void setP2(int x, int y) {
+		P2 = new Point2f(x,y);
+		calculateCoefficients();
+	}
+	
+	public void setP2(Point2f p) {
+		P2 = p;
+		calculateCoefficients();
 	}
 	
 	public float getA() {
@@ -36,14 +66,8 @@ public class LineSeg {
 	public float getC() {
 		return c;
 	}
-	public float getLength() {
+	public double getLength() {
 		return length;
 	}
-	public Point2f getP1() {
-		return P1;
-	}
 	
-	public Point2f getP2() {
-		return P2;
-	}
 }
